828d5d44b8ec7dfb8433977ff218cbe55a853d88
dmxyws <mr_lee_007@sina.com>
2017-11-04 12:33:01 +0800

    footer布局完成，背景图兼容完成，footer高度待处理

--- src/components/footer/footer-nav.vue
+++ src/components/footer/footer-nav.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    底部导航
+  <div class="footer-wrap">
+    <ul class="nav-list">
+      <li class="nav-item"><a href="javascript:;" class="index-a"></a></li>
+      <li class="nav-item"><a href="javascript:;" class="type-a"></a></li>
+      <li class="nav-item"><a href="javascript:;" class="cart-a"></a></li>
+      <li class="nav-item"><a href="javascript:;" class="myepet-a"></a></li>
+    </ul>
   </div>
 </template>
 
@@ -8,6 +13,28 @@
   export default{}
 </script>
 
-<style>
-
+<style lang="stylus" ref="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .footer-wrap
+    width: 100%
+    .nav-list
+      overflow hidden
+      width 100%
+      .nav-item
+        position relative
+        float left
+        width: 25%
+        height 55px
+        .index-a
+          vertical()
+          ft-bg-image("./img/img.png",0,0)
+        .type-a
+          vertical()
+          ft-bg-image("./img/img.png",-85px,-40px)
+        .cart-a
+          vertical()
+          ft-bg-image("./img/img.png",-85px,-80px)
+        .myepet-a
+          vertical()
+          ft-bg-image("./img/img.png",-85px,-120px)
 </style>
